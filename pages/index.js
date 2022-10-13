ce764f893edf4ff6cec22586c4b6ba7a1805ede6
boymax <ujam1101>
2022-10-13 09:14:21 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <h1 className="text-3xl font-bold">NextMall</h1>
+    </Layout>
+  )
+}
