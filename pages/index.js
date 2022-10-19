bf8e2462ffd495d3282b2e6ec2f507039f3ea2ea
“ujam1101” <boymax@naver.com>
2022-10-19 23:54:32 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import ProducItem from '../components/ProducItem'
 export default function Home() {
   return (
     <Layout title="Home">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:grid-cols-2 2xl:grid-cols-6">
         {data.products.map((product) => (
           <ProducItem product={product} key={product.slug}></ProducItem>
         ))}
