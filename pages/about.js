5bf3160ba52f53aa1254aab238d33b1bf790767d
ujam1101 <boymax4622@gmail.com>
2022-12-13 06:35:30 +0900

    a

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default function about() {
-  const name = 'ㅇㅇ'
+  const name = '권도윤'
   return (
     <div>
-      <p>안녕하세요 {name} 님! </p>
+      <p>92113451학번 {name}입니다! </p>
     </div>
   )
 }
